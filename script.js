1cb7a04cb448ceabfdb1dee62a538420322691ec
Prathameshbhujbal <bhujbal.prad@gmail.com>
2024-01-24 13:36:23 +0530

    new works properly

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("It works!!!!!")
+let input = prompt("What would you like to do?");
+let list = [];
+let i = 0;
+while (input !== "quit"){
+    if (input === "new"){
+        let task = prompt("Enter the task you want to add: ");
+        list.push(task)
+    } else if (input === "list"){
+        for (let task of list){
+            console.log(`${i} : ${task}`);
+        }
+    } else if (input === "delete"){
+        let task = prompt("Enter the index of the task you want to delete: ");
+
+    }
+
+    input = prompt("What would you like to do?");
+
+}
